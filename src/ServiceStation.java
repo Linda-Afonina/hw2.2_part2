0f4745fcbc239ab0fec4b2623a369e7ab4e922e3
linda <charming.linda85@gmail.com>
2024-04-26 12:32:24 +0300

    Внесены корректировки: доработан метод check()

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,8 +1,10 @@
-public class ServiceStation {
+public class ServiceStation{
 
     public void check(Transport transport) {
         if (transport != null) {
             System.out.println("Обслуживаем " + transport.modelName);
+            transport.checkTrailer();
+            transport.checkEngine();
             for (int i = 0; i < transport.wheelsCount; i++) {
                 transport.updateTyre();
             }
